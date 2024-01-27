ccd7824f41810629d0452bc8e400510793908ebd
LalithAdithyanSuresh <lalithadithyansuresh@gmail.com>
2024-01-27 14:33:05 +0530

    Adding the a new variable temp to store users input

--- Sem-2/CS1804T - Data Structures Lab/Lab_1_(24_1_24)/main.cpp
+++ Sem-2/CS1804T - Data Structures Lab/Lab_1_(24_1_24)/main.cpp
@@ -1,15 +1,19 @@
-//To find the sum of first n numbers
+//To find the sum of n numbers given by the user
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(){
-	int *sum,*n;
+	int *sum,*n,*temp;
 	n = (int *)malloc(sizeof(int));
 	sum = (int *)malloc(sizeof(int));
+	temp = (int *)malloc(sizeof(int));
+	*sum = 0;
 	printf("\n Enter the value of n : ");
 	scanf("%d",n);
+	printf("Enter the integers : ");
 	while(*n != 0){
-		*sum += *n;
+		scanf("%d", temp);
+		*sum += *temp;
 		*n-=1;
 	}
 	printf("\n The sum is : %d\n",*sum);
