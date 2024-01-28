266b6d534c5dca6cfe8345f901f7f7894e291af8
LalithAdithyanSuresh <lalithadithyansuresh@gmail.com>
2024-01-28 15:46:41 +0530

    Pascal's Triangle Program Completetion

--- Sem-2/CS1002 - Python/BasedOnDate/24_01_2024/PracticeProgram10.py
+++ Sem-2/CS1002 - Python/BasedOnDate/24_01_2024/PracticeProgram10.py
@@ -8,12 +8,39 @@
 # 1 5 10 10 5 1
 
 
-numberOfRows = int(input("Enter the number of rows"))
-lastRow = [1]
-currentRow = []
-for i in range(numberOfRows):
-    if(len(lastRow)==1):
-        currentRow.append(lastRow[0])
-        print(" "*(numberOfRows-i),currentRow[0])
-        break
-    
+numberOfRows = int(input("Enter the number of rows : "))
+MaxLenOfInt = 0
+
+# Intitialising Matrix of Size N x (N*2)-1 with zeros
+Matrix = []
+for row in range(numberOfRows):
+    NewRow = []
+    for column in range((numberOfRows * 2)-1):
+        NewRow.append(0)
+    Matrix.append(NewRow)
+
+# Starting value of 1 in the middle of the matrix
+Matrix[0][numberOfRows-1] = 1
+
+for RowNum , row in enumerate(Matrix):
+    for ColNum , col in enumerate(row):
+        if(col != 0):
+            try:
+                Matrix[RowNum+1][ColNum-1] += col
+            except:
+                pass
+            try:
+                Matrix[RowNum+1][ColNum +1] +=col
+            except:
+                pass
+        if(len(str(col))>MaxLenOfInt):  #Updating the Max int length
+            MaxLenOfInt = len(str(col))
+
+# Printing Matrix by replacing zeros with empty spaces
+for i in Matrix:
+    for j in i:
+        if(j!=0):
+            print(str(j).rjust(MaxLenOfInt),end="")
+        else:
+            print(" "*MaxLenOfInt,end="")
+    print()
