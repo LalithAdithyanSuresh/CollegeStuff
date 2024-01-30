4c280b4a9b4eae41328b34fa06a9f892167d8ccb
LalithAdithyanSuresh <lalithadithyansuresh@gmail.com>
2024-01-30 09:17:21 +0530

    Added Comments

--- Sem-2/CS1002 - Python/BasedOnDate/30_01_2024/PatternGen2.py
+++ Sem-2/CS1002 - Python/BasedOnDate/30_01_2024/PatternGen2.py
@@ -1,5 +1,9 @@
 #Inverse Pattern
-
+#          * 
+#        * * 
+#      * * *
+#    * * * *
+#  * * * * *
 n = int(input("Enter the number of rows : "))
 for i in range(1,n+1):
     print("  "*(n-i),"* "*i)
