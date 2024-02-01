8756ee88c7ab8b37dbcd1f841ff185c43372efec
LalithAdithyanSuresh <lalithadithyansuresh@gmail.com>
2024-02-01 08:35:11 +0530

    pythonPascalWihtoutListAndFactorials

--- Sem-2/CS1002 - Python/BasedOnTopic/PatternGen/PatternGen5.1.py
+++ Sem-2/CS1002 - Python/BasedOnTopic/PatternGen/PatternGen5.1.py
@@ -9,3 +9,17 @@
 
 # without any lists
 
+n = int(input("Enter the number of rows : "))
+pval = 1
+print(" "*(n-1),1)
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(i+1):
+        if(j == 0 or j==i):
+            print("1",end = " ")
+            pval = 1
+        else:
+            print("{}".format((pval*(i-j+1))//j),end=" ")
+            pval = (pval*(i-j+1))//j
+    print()
+        
