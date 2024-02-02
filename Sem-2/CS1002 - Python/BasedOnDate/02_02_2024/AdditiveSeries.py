232817f88e5bf0d49f6a579764e6ae4bcd2316c2
LalithAdithyanSuresh <lalithadithyansuresh@gmail.com>
2024-02-02 14:08:55 +0530

    DataStructures

--- Sem-2/CS1002 - Python/BasedOnDate/02_02_2024/AdditiveSeries.py
+++ Sem-2/CS1002 - Python/BasedOnDate/02_02_2024/AdditiveSeries.py
@@ -1,10 +1,10 @@
 #  Series 1+ 1+2  +1+2+3+ 1+2+3+4....
 
-N = int(input("Enter the End of Series : "));
-c = 0
-for i in range(1,N+1):
-    for j in range(i+1):
-        c+=j
-print(c,end=" ")
-print(int((N/6)*(N+1)*(N+2)))
+N = int(input("Enter the End of Series : "));print(int((N/6)*(N+1)*(N+2)))
+# c = 0
+# for i in range(1,N+1):
+#     for j in range(i+1):
+#         c+=j
+# print(c,end=" ")
+# print(int((N/6)*(N+1)*(N+2)))
 
