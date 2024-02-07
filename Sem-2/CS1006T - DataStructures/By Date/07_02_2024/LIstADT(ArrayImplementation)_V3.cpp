f2e1a0bf59fd182ea41f792184bd439f3415cbeb
LalithAdithyanSuresh <lalithadithyansuresh@gmail.com>
2024-02-07 09:41:56 +0530

    NOw

---
+++ Sem-2/CS1006T - DataStructures/By Date/07_02_2024/LIstADT(ArrayImplementation)_V3.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#define List_Size_Given 5
+
+class List{
+    private:
+        int List_Size = List_Size_Given;
+        int CurrentIndex = -1;
+        int TempCurrent = 0;
+        int listArray[List_Size_Given] ={0};
+
+    public: 
+        int IsEmpty(){
+            if (CurrentIndex == -1){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+        int IsFull(){
+            if(CurrentIndex == List_Size-1){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+        void Append(int val){
+            if(IsFull()==0){
+                for(TempCurrent = CurrentIndex;TempCurrent > -1;TempCurrent--){
+                    listArray[TempCurrent+1]=listArray[TempCurrent];
+                }
+                listArray[0]=val;
+                CurrentIndex++;
+                // Display();
+                // printf("\nInserted %d\n",val);
+            }
+        }
+        void Display(){
+            for(TempCurrent=CurrentIndex;TempCurrent>-1;TempCurrent--){
+                printf("%d,",listArray[TempCurrent]);
+            }
+        }
+};
+
+int main(){
+    List a;
+    int opt,var;
+    while(1==1){
+        printf("Options : \n\t1-Append\n>>>");
+        scanf("%d",opt);
+        switch (opt)
+        {
+        case 0:
+            return 0;
+        case 1:
+            printf("\nValue : ");
+            scanf("%d",&var);
+            a.Append(var);
+            break;
+        case 2:
+            printf("Values in List : ");
+            a.Display();
+        default:
+            break;
+        }
+    }
+}
---
+++ Sem-2/CS1006T - DataStructures/By Date/07_02_2024/ListADT(ArrayImplementation)_V2.py
@@ -0,0 +1,25 @@
+# List > ADT
+
+# Abstaraction > Complexity is hidden
+
+class List:
+    def __init__(self):
+        self.current = 0
+        self.size = 4
+    def IsEmpty(self):
+        return self.current== 0
+    def IsFull(self):
+        return self.current == self.size-1
+    def Insertion(self,val):
+        if(not self.IsFull()):
+            return 1
+        else:
+            return 0
+a = List()
+print(a.Insertion(1))
+        
+
+
+
+
+
