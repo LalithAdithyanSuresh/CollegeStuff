6d3cb7c47a4dce5b87f0d023868f9c15662bd5ea
LalithAdithyanSuresh <lalithadithyansuresh@gmail.com>
2024-02-08 08:41:27 +0530

    Corrected Naming Schemes

--- Sem-2/CS1002 - Python/BasedOnDate/06_02_2024/Strings_Q6_FrequencyOfEachChar.py
+++ Sem-2/CS1002 - Python/BasedOnDate/06_02_2024/Strings_Q6_FrequencyOfEachChar.py
@@ -6,3 +6,12 @@ for i in input("Enter the String : "):
     else:CharCount[i] = 1
 for i,j in CharCount.items():
     print(i,' : ',j)
+
+# without list,dicts
+b = input("Enter the string :")
+s =''
+for i in b:
+    if i not in s:
+        s+=i
+        print(i," : ",b.count(i))
+    
