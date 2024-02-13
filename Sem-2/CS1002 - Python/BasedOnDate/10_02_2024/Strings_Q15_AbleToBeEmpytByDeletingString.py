a40c6e69ad6ee0e5d9052b599c409912052aeab6
LalithAdithyanSuresh <lalithadithyansuresh@gmail.com>
2024-02-13 08:28:42 +0530

    Rexursive

--- Sem-2/CS1002 - Python/BasedOnDate/10_02_2024/Strings_Q15_AbleToBeEmpytByDeletingString.py
+++ Sem-2/CS1002 - Python/BasedOnDate/10_02_2024/Strings_Q15_AbleToBeEmpytByDeletingString.py
@@ -1,8 +1,11 @@
 # Check whether a string can be made empty by recursively deleting a substring
 
 s = input("Enter the String : ")
+
 d = input("Enter Deleting String : ")
 while(d in s):
     s=s.replace(d,'')
 if s=='':print("Yes")
 else:print('No')
+
+
