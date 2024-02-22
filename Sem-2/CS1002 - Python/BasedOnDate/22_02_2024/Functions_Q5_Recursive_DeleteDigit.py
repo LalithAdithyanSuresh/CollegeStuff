6d5cad51139e173f1d249e0d22b006b13f83f16b
LalithAdithyanSuresh <lalithadithyansuresh@gmail.com>
2024-02-22 09:01:08 +0530

    Made return as int in Deleting a digit Program

--- Sem-2/CS1002 - Python/BasedOnDate/22_02_2024/Functions_Q5_Recursive_DeleteDigit.py
+++ Sem-2/CS1002 - Python/BasedOnDate/22_02_2024/Functions_Q5_Recursive_DeleteDigit.py
@@ -4,8 +4,8 @@ def DeleteDigit(N,pos):
     if(len(str(N))==0):
         return ''
     if(pos == 0):
-        return DeleteDigit(str(N)[1:],pos-1)
+        return int(DeleteDigit(str(N)[1:],pos-1))
     else:
-        return str(N)[0] + DeleteDigit(str(N)[1:],pos-1)
+        return int(str(N)[0] + str(DeleteDigit(str(N)[1:],pos-1)))
 
 print(DeleteDigit(int(input("Enter the integer : ")),int(input("Enter the position to remove : "))))
