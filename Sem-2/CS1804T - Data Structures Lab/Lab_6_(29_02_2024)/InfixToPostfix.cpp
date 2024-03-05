90660aa4eeb55f7d187cdaa85578656f7728d5a4
Lalith Adithyan Suresh <140822743+LalithAdithyanSuresh@users.noreply.github.com>
2024-03-05 23:45:32 +0530

    finish!

--- Sem-2/CS1804T - Data Structures Lab/Lab_6_(29_02_2024)/InfixToPostfix.cpp
+++ Sem-2/CS1804T - Data Structures Lab/Lab_6_(29_02_2024)/InfixToPostfix.cpp
@@ -8,17 +8,18 @@ struct Variable{
     int val;
 };
 
-int Valueof(char vari,int *VarCount,struct Variable *vars);
-int ChangeValueof(char vari,int value,int *VarCount,struct Variable *vars);
+int Valueof(char vari,int *VarCount,struct Variable **vars);
+int ChangeValueof(char vari,int value,int *VarCount,struct Variable **vars);
 
 int main(){
     struct Variable *Vars=nullptr;
-    int opt=1,tempPrec,stackPrec,*VarCount = 0;
+    int opt=1,tempPrec,stackPrec,EXPlen=0;
+    int VarCount = 0;
     char temp,c;
     List Exp,Pfix;
     Stack OpStack;
+    INTStack Answer;
     enum Prec {assign,addsub,muldivmod};
-    enum Asso {LR,RL};
     while (opt!=0){
         printf("Options:\n\t1 - Input Expression\n\t2 - Infix to Postfix\n>>>");
         scanf("%d",&opt);
@@ -29,9 +30,24 @@ int main(){
             printf("Enter Your Expression >> ");
             while ((c = getchar()) != '\n' && c != EOF) {
                     Exp.Append_Node(c);
+                    EXPlen++;
+            }
+            printf("Getting values of variables >>\n");
+            while(EXPlen>0){
+                temp = Exp.DisplayFront();
+                Exp.Append_Node(temp);
+                Exp.Delete_Beg_Node();
+                if(!(temp=='*' || temp=='/' || temp=='%' || temp=='+' || temp=='-' || temp == '=')){
+                    if(Valueof(temp,&VarCount,&Vars) == 0){
+                        printf("Value of %c : ",temp);
+                        scanf("%d",&opt);
+                        ChangeValueof(temp,opt,&VarCount,&Vars);
+                    }
                 }
+                EXPlen--;
+            }
+            printf("\nSaved Variables\n\n");
             break;
-            
         case 2:
             while((c = Exp.DisplayFront()) != '~'){
                 if(c=='*' || c=='/' || c=='%')tempPrec=muldivmod;
@@ -86,38 +102,43 @@ int main(){
         case 3:
         char ans;
             while((temp = Pfix.DisplayFront())!='~'){
-                if(temp=='*' || temp=='/' || temp=='%' || temp=='+' || temp=='-' || temp == '='){
+                if(temp=='*' || temp=='/' || temp=='%' || temp=='+' || temp=='-'){
 
-                    stackPrec=(int)OpStack.Display() - 48;
-                    OpStack.Delete_Beg_Node();
-                    tempPrec = (int)OpStack.Display() - 48;
-                    OpStack.Delete_Beg_Node();
+                    stackPrec=Answer.Display();
+                    Answer.Delete_Beg_Node();
+                    tempPrec = Answer.Display();
+                    Answer.Delete_Beg_Node();
+                }else if(temp=='='){
+                    stackPrec = Answer.Display();
+                    Answer.Delete_Beg_Node();
                 }
                 switch (temp)
                 {
                 case '*':
-                    ans = (char)(tempPrec * stackPrec + 48);
+                    ans = tempPrec * stackPrec;
                     break;
                 case '/':
-                    ans = (char)(tempPrec / stackPrec + 48);
+                    ans = tempPrec / stackPrec;
                     break;
                 case '%':
-                    ans = (char)(tempPrec % stackPrec + 48);
+                    ans = tempPrec % stackPrec;
                     break;
                 case '+':
-                    ans = (char)(tempPrec + stackPrec + 48);
+                    ans = tempPrec + stackPrec;
                     break;
                 case '-':
-                    ans = (char)(tempPrec - stackPrec + 48);
+                    ans = tempPrec - stackPrec;
                     break;
+                case '=':
+                    ans = stackPrec;
                 default:
-                    ans = temp;
+                    ans = Valueof(temp,&VarCount,&Vars);
                     break;
                 }
-                OpStack.Insert_Beg_Node(ans);
+                Answer.Insert_Beg_Node(ans);
                 Pfix.Delete_Beg_Node();
             }
-            printf("%c",OpStack.Display());
+            printf("%d",Answer.Display());
             break;
         default:
             break;
@@ -129,29 +150,30 @@ int main(){
 
 }
 
-
-int Valueof(char vari,int *VarCount,struct Variable *vars){
+// To get the value of a variable if it is available
+int Valueof(char vari,int *VarCount,struct Variable **vars){
     for (int i = 0; i < *VarCount; i++) {
-        if (vars[i].var == vari) {
-            return vars[i].val;
+        if ((*vars)[i].var == vari) {
+            return (*vars)[i].val;
         }
     }
     return 0;
 }
 
-int ChangeValueof(char vari,int value,int *VarCount,struct Variable *vars){
+// To change a variables value if it exists else create a new variable structure and store value
+int ChangeValueof(char vari,int value,int *VarCount,struct Variable **vars){
     for (int i = 0; i < *VarCount; i++) {
-        if (vars[i].var == vari) {
-            vars[i].val = value;
+        if ((*vars)[i].var == vari) {
+            (*vars)[i].val = value;
             return 1;
         }
     }
     (*VarCount)++;
-    vars = (struct Variable *)realloc(vars, (*VarCount) * sizeof(struct Variable));
-    if (vars == nullptr) {
+    *vars = (struct Variable *)realloc(*vars, (*VarCount) * sizeof(struct Variable));
+    if (*vars == nullptr) {
         return 0;
     }
-    vars[*VarCount - 1].var = vari;
-    vars[*VarCount - 1].val = value;
+    (*vars)[*VarCount - 1].var = vari;
+    (*vars)[*VarCount - 1].val = value;
     return 1;
 }
--- Sem-2/CS1804T - Data Structures Lab/Lab_6_(29_02_2024)/StackADTUsingSLL.h
+++ Sem-2/CS1804T - Data Structures Lab/Lab_6_(29_02_2024)/StackADTUsingSLL.h
@@ -1,7 +1,3 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-
 class Stack{
     private:
         struct Node{
@@ -69,5 +65,78 @@ char Stack::Display(){
         return Head->data;
     }else{
         return '~';
+    }
+}
+
+
+// ----------------------------------------------------INTEGER STACK-------------------------------------------
+
+class INTStack{
+    private:
+        struct Node{
+            int data;
+            struct Node *Next;
+        };
+        struct Node *Head;
+        struct Node *CurrentPoint;
+        struct Node *NewNode;
+    public:
+        INTStack(){
+            Head = nullptr;
+        }
+        struct Node * CreateNode();
+        void DeleteNode(struct Node *del);
+        int IsEmpty();
+        int Insert_Beg_Node(int val);
+        int Delete_Beg_Node();
+        int Display();
+};
+// Create New Node And return Pointer
+struct INTStack::Node * INTStack::CreateNode(){
+    return (struct Node *)malloc(sizeof(struct Node));
+}
+
+// Delete and free memoryof the Node
+void INTStack::DeleteNode(struct Node *del){
+    free(del);
+}
+
+// Check if the Stack is empty
+int INTStack::IsEmpty(){
+    if (Head==nullptr){
+        return 1;
+    }else{
+        return 0;
+    }
+    
+}
+
+// Insert a New Node in the beginning
+int INTStack::Insert_Beg_Node(int val){
+    NewNode = CreateNode();
+    NewNode->data = val;
+    NewNode->Next = Head;
+    Head = NewNode;
+    return 1;
+}
+
+// Delete the beginning Node
+int INTStack::Delete_Beg_Node(){
+    if(IsEmpty()==0){
+        CurrentPoint = Head;
+        Head = CurrentPoint->Next;
+        DeleteNode(CurrentPoint);
+        return 1;
+
+    }else{
+        return 0;
+    }
+}
+// Display the top of the Stack of the Stack
+int INTStack::Display(){
+    if(IsEmpty()==0){
+        return Head->data;
+    }else{
+        return 0;
     }
 }
