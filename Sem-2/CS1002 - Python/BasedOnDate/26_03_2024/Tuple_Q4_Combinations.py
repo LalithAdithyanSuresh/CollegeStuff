5492966597478ed3c789e46a1b18f778303bb8b5
Lalith Adithyan Suresh <140822743+LalithAdithyanSuresh@users.noreply.github.com>
2024-03-26 09:42:35 +0530

    SingleLine

--- Sem-2/CS1002 - Python/BasedOnDate/26_03_2024/Tuple_Q4_Combinations.py
+++ Sem-2/CS1002 - Python/BasedOnDate/26_03_2024/Tuple_Q4_Combinations.py
@@ -1,8 +1,2 @@
-def Comb(l):
-    return [(a,b,c,d) for a in l for b in l for c in l for d in l if (a*b==c*d and a!=b and a!=c and a!=d and b!=c and b!=d and c!=d)]
-
-
-
-
-
-print(Comb(eval(input("Enter a list of elements : "))))
+l = eval(input("Enter The list : "))
+print([(a,b,c,d) for a in l for b in l for c in l for d in l if (a*b==c*d and a!=b and a!=c and a!=d and b!=c and b!=d and c!=d)])
