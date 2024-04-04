65f96969fefdef82d205375b39393dafbdd5b874
Lalith Adithyan Suresh <140822743+LalithAdithyanSuresh@users.noreply.github.com>
2024-04-04 09:30:25 +0530

    Updated Code to be  more space efficient

--- Sem-2/CS1002 - Python/BasedOnDate/02_04_2024/Dict_Q13_DictionaryOfListToLisOfDictionary.py
+++ Sem-2/CS1002 - Python/BasedOnDate/02_04_2024/Dict_Q13_DictionaryOfListToLisOfDictionary.py
@@ -2,11 +2,9 @@
 
 ori = eval(input("Enter the Dictionary : "))
 out = []
-c = 0
 for i in range(len(ori[list(ori.keys())[0]])):
     temp = {}
     for j in ori:
-        temp[j] = ori[j][c]
-    c+=1
+        temp[j] = ori[j][i]
     out.append(temp.copy())
 print(out)
