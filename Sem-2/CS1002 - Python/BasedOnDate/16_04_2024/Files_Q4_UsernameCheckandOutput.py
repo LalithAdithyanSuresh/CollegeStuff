c1720f8a7d0361e9c44e2e9f2619a1bdaf379ece
Lalith Adithyan Suresh <140822743+LalithAdithyanSuresh@users.noreply.github.com>
2024-04-16 15:06:21 +0530

    kn

--- Sem-2/CS1002 - Python/BasedOnDate/16_04_2024/Files_Q4_UsernameCheckandOutput.py
+++ Sem-2/CS1002 - Python/BasedOnDate/16_04_2024/Files_Q4_UsernameCheckandOutput.py
@@ -8,6 +8,7 @@ if(len(sys.argv) !=3):
 InputFolder , OutputFolder = sys.argv[1],sys.argv[2]
 
 domain = "@snuchennai.edu.in"
+ptr = "^[A-Za-z][A-Za-z0-9\.-_]*$"
 
 # Input Folder Check
 try:
@@ -22,7 +23,7 @@ try:
     if(len(OutputFiles)>1):
         print("Files already exist in the Output Folder")
         if(input("\nDo you want to overwrite the folder's contents? (y/n)") == 'y'):
-            os.remove(OutputFolder)
+            os.rmdir(OutputFolder)
         else:
             print("Program exiting")
             exit()
@@ -34,5 +35,25 @@ for i in InputFiles:
     CurrFile = open(InputFolder+'/'+i)
     usernames = CurrFile.readlines()
     CurrFile.close()
+    for j in range(len(usernames)):
+        user = usernames[j]
+        if(user in re.findall(user,ptr)):
+            pass
+        else:
+            user = list(user)
+            valid = ''
+            for k in range(len(user)):
+                if(user[k].isalnum() or user[k]=='.' or user[k]=='_' or user[k]=='-'):
+                    valid+=user[k]
+                elif(user[k]=='@'):
+                    break
+                else:
+                    continue
+            user = valid
+        user+=domain
+        usernames[j]=user
+    outFile = open(OutputFolder+'/'+i,'w')
     for i in usernames:
-        
+        outFile.write(i+'\n')
+    outFile.close()
+            
