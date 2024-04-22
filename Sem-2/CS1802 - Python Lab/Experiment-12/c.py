d9bf5c132fbe6e9897c66fc0e5553e6ae383675c
Lalith Adithyan Suresh <140822743+LalithAdithyanSuresh@users.noreply.github.com>
2024-04-22 20:35:49 +0530

    : )

--- Sem-2/CS1802 - Python Lab/Experiment-12/c.py
+++ Sem-2/CS1802 - Python Lab/Experiment-12/c.py
@@ -3,6 +3,13 @@ args = sys.argv
 if len(args)!=2:
     exit()
 
-for i in range(2,args[1]):
-    for j in range(3,i//2):
-        if
+for i in range(2,int(args[1])):
+    f = 0
+    for j in range(2,i//2+1):
+        if(i%j==0):
+            f = 1
+            break
+    if f == 0:
+        print(i)
+
+    
