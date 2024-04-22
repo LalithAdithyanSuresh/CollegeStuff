10422bbd9c19d75172eb45d4a465d92b281a5cb3
Lalith Adithyan Suresh <140822743+LalithAdithyanSuresh@users.noreply.github.com>
2024-04-22 21:42:44 +0530

    wer

--- Sem-2/CS1704 - DataScience/LetterReognition/LetterRec.py
+++ Sem-2/CS1704 - DataScience/LetterReognition/LetterRec.py
@@ -1,17 +1,21 @@
-from sqlalchemy import column
 import streamlit as st
+from sklearn.neighbors import KNeighborsClassifier
 import pickle
+import numpy as np
 
 file = open("BestModel.pkl",'rb')
 dataPackage = pickle.load(file)
-k_value = dataPackage[1]
+
 model = dataPackage[0]
-columns = dataPackage[3]
+k_value = dataPackage[1]
 score = dataPackage[2]
+columns = dataPackage[3]
+
 st.write("Using K-Value : {}".format(k_value))
 st.write("Model Accuracy : {}".format(score))
-val = [0 for x in range(len(columns))]
-for i in range(len(columns)):
-    val[i] = st.number_input(columns[i],0,15)
 
-st.text("Hello")
+val = [0 for _ in range(len(columns[1:]))]
+for i, column_name in enumerate(columns[1:]):
+    val[i] = st.number_input(column_name, 0, 15)
+    
+st.write("Model Predicted Letter : {}".format(model.predict(np.array(val).reshape(1, -1))[0]))
